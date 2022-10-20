307486e7555900965d75df470fbd9e1742ce1091
Seif Ahmed <seif.ahmed@softwire.com>
2022-10-20 12:12:33 +0100

    ADD: Command line interface

---
+++ src/cli.ts
@@ -0,0 +1,52 @@
+import readlineSync from "readline-sync";
+import { Location } from "./api/Location";
+
+function getString(prompt: string) {
+    const input = readlineSync.question(prompt);
+    return input.toLowerCase();
+}
+
+function getLocationsFromUser(locations: Location[]) {
+    const userInputLocation = getString("Enter location name: ");
+    return locations.filter(
+        (location) => location.name.toLowerCase() === userInputLocation
+    );
+}
+
+function chooseLocationFromMatches(locations: Location[]): Location {
+    console.log("Multiple locations found!");
+
+    locations.forEach((location) => {
+        console.log(location.toString());
+    });
+
+    const id = getString("Enter ID: ");
+
+    const match = locations.find((location) => location.id === id);
+
+    if (!match) {
+        return chooseLocationFromMatches(locations);
+    }
+
+    return match;
+}
+
+function handleUserLocation(location: Location) {
+    console.log(location.toString());
+}
+
+export function commandLineInterface(locations: Location[]) {
+    const matches = getLocationsFromUser(locations);
+
+    if (matches.length === 0) {
+        console.log("No location found!");
+        return;
+    }
+
+    if (matches.length > 1) {
+        const match = chooseLocationFromMatches(matches);
+        return handleUserLocation(match);
+    }
+
+    return handleUserLocation(matches[0]);
+}
