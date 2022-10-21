8e99969cb235e11a506aa83f5ef94f40572bde28
Seif Ahmed <seif.ahmed@softwire.com>
2022-10-21 17:19:16 +0100

    Added weatherType description map

--- components/Dashboard.tsx
+++ components/Dashboard.tsx
@@ -4,6 +4,39 @@ import { Dial } from "./Dial";
 import styles from "./Dashboard.module.css";
 import { UmbrellaFill, ThermometerHalf } from "react-bootstrap-icons";
 
+const weatherTypes: { [key: string]: string } = {
+	NA: "not available",
+	"0": "clear",
+	"1": "sunny",
+	"2": "partly cloudy",
+	"3": "partly cloudy",
+	"4": "not used (probably just want it to be not available too)",
+	"5": "mist",
+	"6": "fog",
+	"7": "cloudy",
+	"8": "overcast",
+	"9": "light rain",
+	"10": "light rain",
+	"12": "light rain",
+	"11": "drizzle",
+	"13": "heavy rain",
+	"14": "heavy rain",
+	"15": "heavy rain",
+	"16": "sleet showers",
+	"17": "heavy rain",
+	"18": "heavy rain",
+	"19": "hail",
+	"20": "hail",
+	"21": "hail",
+	"22": "light snow",
+	"23": "light snow",
+	"24": "light snow",
+	"25": "heavy snow",
+	"26": "heavy snow",
+	"27": "heavy snow",
+	"28": "thunder",
+};
+
 const Dashboard: React.FC<{
 	location: APILocation;
 	dataPoint: APIDataPoint;
@@ -20,7 +53,7 @@ const Dashboard: React.FC<{
 				<Dial amount={dataPoint.data.temperature} size={8}>
 					<ThermometerHalf size={48} />
 				</Dial>
-				<p>{dataPoint.data.weatherType}</p>
+				<p>{weatherTypes[dataPoint.data.weatherType]}</p>
 				<Dial amount={dataPoint.data.maxUVIndex} label="UV" size={8} />
 			</div>
 		</div>
