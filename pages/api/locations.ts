4c274c36c1a7900426a5c6e48e4de71143acbe65
OliverSoftwire <115625185+OliverSoftwire@users.noreply.github.com>
2022-10-21 14:04:38 +0100

    Add locations endpoint

---
+++ pages/api/locations.ts
@@ -0,0 +1,28 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import getConfig from "next/config";
+import { handleErrors } from "lib/server/errorHandler";
+import { Location, LocationJSON } from "lib/server/Location";
+import MetOfficeAPI from "lib/server/MetOfficeAPI";
+
+const { serverRuntimeConfig } = getConfig();
+
+export default handleErrors(async function handler(
+	req: NextApiRequest,
+	res: NextApiResponse
+) {
+	console.log("Locations requested");
+
+	const locations = await MetOfficeAPI.getLocations();
+
+	const filterKeys = Object.keys(req.query).filter((filterKey) =>
+		Location.hasProperty(filterKey)
+	) as (keyof LocationJSON)[];
+
+	const filteredLocations = locations.filter((location) =>
+		filterKeys.every(
+			(filterKey) => req.query[filterKey] === location[filterKey]
+		)
+	);
+
+	res.status(200).json(filteredLocations);
+});
