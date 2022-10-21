da016bcc8a7e0ec9e071ae37b43ccd03eec70708
OliverSoftwire <115625185+OliverSoftwire@users.noreply.github.com>
2022-10-21 11:48:45 +0100

    Re-add prettier config
    Convert indentation to tabs

--- pages/api/getForecast.ts
+++ pages/api/getForecast.ts
@@ -7,46 +7,44 @@ import MetOfficeAPI from "../../lib/server/MetOfficeAPI";
 const { serverRuntimeConfig } = getConfig();
 
 export default handleErrors(async function handler(
-    req: NextApiRequest,
-    res: NextApiResponse
+	req: NextApiRequest,
+	res: NextApiResponse
 ) {
-    console.log("Forecasts requested");
+	console.log("Forecasts requested");
 
-    const locations = await MetOfficeAPI.getLocations();
+	const locations = await MetOfficeAPI.getLocations();
 
-    const filterKeys = Object.keys(req.query).filter((filterKey) =>
-        Location.hasProperty(filterKey)
-    ) as (keyof LocationJSON)[];
+	const filterKeys = Object.keys(req.query).filter((filterKey) =>
+		Location.hasProperty(filterKey)
+	) as (keyof LocationJSON)[];
 
-    const filteredLocations = locations.filter((location) =>
-        filterKeys.every(
-            (filterKey) => req.query[filterKey] === location[filterKey]
-        )
-    );
+	const filteredLocations = locations.filter((location) =>
+		filterKeys.every(
+			(filterKey) => req.query[filterKey] === location[filterKey]
+		)
+	);
 
-    if (filteredLocations.length > serverRuntimeConfig.locationLimit) {
-        return res.status(400).json({
-            message:
-                "Too many locations matching parameters. Please be more specific.",
-        });
-    }
+	if (filteredLocations.length > serverRuntimeConfig.locationLimit) {
+		return res.status(400).json({
+			message:
+				"Too many locations matching parameters. Please be more specific.",
+		});
+	}
 
-    const forecasts = filteredLocations.map(async (location) => {
-        const forecastData = await MetOfficeAPI.getLocationForecast(location);
+	const forecasts = filteredLocations.map(async (location) => {
+		const forecastData = await MetOfficeAPI.getLocationForecast(location);
 
-        const dataPoints = forecastData.periods
-            .map((period) =>
-                period.dataPoints.map((dataPoint) => dataPoint.toJSON())
-            )
-            .flat();
+		const dataPoints = forecastData.periods
+			.map((period) => period.dataPoints.map((dataPoint) => dataPoint.toJSON()))
+			.flat();
 
-        return {
-            id: location.id,
-            name: location.name,
-            area: location.area,
-            forecast: dataPoints,
-        };
-    });
+		return {
+			id: location.id,
+			name: location.name,
+			area: location.area,
+			forecast: dataPoints,
+		};
+	});
 
-    res.status(200).json(await Promise.all(forecasts));
+	res.status(200).json(await Promise.all(forecasts));
 });
