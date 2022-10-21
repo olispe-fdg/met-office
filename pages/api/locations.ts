f469b54207db6c324d9e392a07c35020903eddd5
OliverSoftwire <115625185+OliverSoftwire@users.noreply.github.com>
2022-10-21 14:05:24 +0100

    Prune unused

--- pages/api/locations.ts
+++ pages/api/locations.ts
@@ -1,10 +1,7 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import getConfig from "next/config";
 import { handleErrors } from "lib/server/errorHandler";
 import { Location, LocationJSON } from "lib/server/Location";
 import MetOfficeAPI from "lib/server/MetOfficeAPI";
-
-const { serverRuntimeConfig } = getConfig();
 
 export default handleErrors(async function handler(
 	req: NextApiRequest,
